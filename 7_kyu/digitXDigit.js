0a49fe5078ff6340b31dd4397f6c82aece45963f
Julia Dannerth <jdannerth@gmail.com>
2023-02-24 20:00:56 -0500

    sumOfNumbers 7kyu added

--- 7_kyu/digitXDigit.js
+++ 7_kyu/digitXDigit.js
@@ -1,9 +1,9 @@
 // DESCRIPTION:
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
-// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+// For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
 
-// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-35)
+// Example #2: An input of 765 will/should return 493625 because 7^2 is 49, 6^2 is 36, and 5^2 is 25. (49-36-35)
 
 // Note: The function accepts an integer and returns an integer.
 
