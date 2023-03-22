7c33c473c5dd2cc5ff2d7a1fca596686a4fda2f4
Julia Dannerth <jdannerth@gmail.com>
2023-03-22 14:12:09 -0400

    mispelledWord 7kyu added

---
+++ 7_kyu/mispelledWord.js
@@ -0,0 +1,65 @@
+// DESCRIPTION:
+// Create a function mispelled(word1, word2):
+
+// mispelled('versed', 'xersed'); // returns true
+// mispelled('versed', 'applb'); // returns false
+// mispelled('versed', 'v5rsed'); // returns true
+// mispelled('1versed', 'versed'); // returns true
+// mispelled('versed', 'versed'); // returns true
+// It checks if the word2 differs from word1 by at most one character.
+
+// This can include an extra char at the end or the beginning of either of words.
+
+// In the tests that expect true, the mispelled word will always differ mostly by one character. If the two words are the same, return True.
+
+//My Solution:
+var mispelled = function(word1, word2) {
+    if (Math.abs(word1.length - word2.length) > 1) {
+      return false;
+    }
+  
+    if (word1 === word2) {
+      return false;
+    }
+  
+    let offByOne = false;
+    let i = 0, j = 0;
+  
+    while (i < word1.length && j < word2.length) {
+      if (word1[i] !== word2[j]) {
+        if (offByOne) {
+          return false;
+        }
+        offByOne = true;
+        if (word1.length > word2.length) {
+          i++;
+        } else if (word1.length < word2.length) {
+          j++;
+        } else {
+          i++;
+          j++;
+        }
+      } else {
+        i++;
+        j++;
+      }
+    }
+  
+    return offByOne || word1.length - word2.length === 1 || word2.length - word1.length === 1;
+  }
+
+  //Cleaner Solution:
+  function mispelled(word1, word2) {
+    if(Math.abs(word1.length - word2.length) >= 2){
+      return false;}
+      
+    if(Math.abs(word1.length - word2.length) == 1){
+      return (word1.includes(word2) || word2.includes(word1)); }  
+   
+   let res = 0;
+    for(let i = 0; i < word1.length; i++){  
+      if(word1[i] !== word2[i]){res += 1;}  
+    }
+    if(res <= 1) { return true;} 
+    else { return false;}  
+  }
