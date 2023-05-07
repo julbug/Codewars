39ce1f793f11ac7c1206d3809e6fb554aba2b208
Julia Dannerth <jdannerth@gmail.com>
2023-05-06 20:19:08 -0400

    persistentBugger 6kyu added

---
+++ 6_kyu/persistentBugger.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+
+
+
+//My Solution:
+function persistence(num) {
+    let count = 0;
+  
+    while (num.toString().length > 1) {
+      let product = 1;
+      for (let i = 0; i < num.toString().length; i++) {
+        const digit = parseInt(num.toString()[i], 10);
+        product *= digit;
+      }
+      num = product;
+      count++;
+    }
+  
+    return count;
+  }
