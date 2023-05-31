20888ac3628249c21bc4200d507065bc1f17bc59
Julia Dannerth <jdannerth@gmail.com>
2023-05-30 23:03:47 -0400

    reversedSequence 8kyu added

---
+++ 8_kyu/reversedSequence.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+//My Solution:
+const reverseSeq = n => {
+    let countdownArray = [];
+    for (let i = n; i >= 1; i--) {
+      countdownArray.push(i);
+    }
+    return countdownArray;
+  }
