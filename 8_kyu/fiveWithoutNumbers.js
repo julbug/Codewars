6080420c9b8eb0a30aebf528dae62237a7a96eb5
Julia Dannerth <jdannerth@gmail.com>
2023-06-06 08:29:31 -0400

    fiveWithoutNumbers 8kyu added

---
+++ 8_kyu/fiveWithoutNumbers.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+
+
+
+//My Solution:
+function unusualFive() {
+    const word = 'hello';
+    const five = word.length;
+    return five;
+  }
+
+  //Cleaner Solution:
+  function unusualFive() {
+    return 'hello'.length
+  }
+  
